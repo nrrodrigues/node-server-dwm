07bf33ffaea34312786579a0752fd03ac0714c71
Nuno Rodrigues <nuno.rodrigues@jci.com>
2020-11-09 15:13:02 +0000

    change port

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.get("/", (req, res) => {
 require("./app/routes/customer.routes.js")(app);
 
 // set port, listen for requests
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
   console.log("Server is running on port 3000.");
 });
